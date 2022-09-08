a41e74919ee1d72958c47c27f41c6f3a4acc5007
adham618 <adhamtarek291@gmail.com>
2022-09-08 22:01:59 +0200

    fix font

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  optimizeFonts: false,
   env: {
     NEXT_PUBLIC_CONTRACTADDRESS: process.env.NEXT_PUBLIC_CONTRACTADDRESS,
     NEXT_PUBLIC_NFTCONTRACTADDRESS: process.env.NEXT_PUBLIC_NFTCONTRACTADDRESS,
